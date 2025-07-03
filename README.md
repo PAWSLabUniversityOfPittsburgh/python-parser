93bdd1d6e6ffd3bf9251a146915cc69c22de751e
Arun Balajiee Lekshmi Narayanan <encodedgeek@gmail.com>
2025-07-03 15:44:25 -0400

    concepts

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This Acos server tool parses Python 3 code and
 returns the found concepts by traversing the AST
 of the code.
+
+
+## Requires 
+Python==3.9.22
+
+## To run frontend
+python3.9 -m streamlit run streamlit_main.py
+
+## To run API
+python3.9 -m uvicorn main_api:app --reload-include="main_api.py" --reload-exclude="*/py-files/*"
---
+++ main_api.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from concepts_parser import main, post_process_parser
+import os
+
+app = FastAPI()
+
+class CodeString(BaseModel):
+    """
+    Custom type for code strings.
+    This can be extended with validation if needed.
+    """
+    content_id: int
+    code_str: str
+    section_id: str
+    resource_id: str
+    is_active: int = 1  # Default value for is_active
+    date_added: str = None  # Default value for date_added
+
+@app.post("/extract_concepts")
+async def extract_concepts(code_json: CodeString):
+    print(code_json)
+    
+    if code_json is None:
+        raise HTTPException(status_code=400, detail="Code string cannot be empty")
+    try:
+        code_str = code_json.code_str.strip()
+        # Create a temporary directory for the code file
+        if not os.path.exists('./py-files'):
+            os.mkdir('py-files')
+
+        # Write the code to a temporary file
+        with open('./py-files/tmp1.py', 'w+') as f:
+            f.write(code_str)
+
+        # Extract concepts from the code
+        codelines, response = main(filename='py-files/tmp1.py')
+        
+        # Clean up the temporary files
+        os.remove('./py-files/tmp1.py')
+        os.rmdir('./py-files')
+
+        response_df =  post_process_parser(response)
+        return response_df.to_dict(orient='records')
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
